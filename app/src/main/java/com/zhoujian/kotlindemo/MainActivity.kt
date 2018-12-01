9b10140eb184fc4a98d1fdb739d3a12656941537
zeke123 <zekezhoujian@163.com>
2018-12-01 12:19:45 +0800

    学习kotlin函数与变量

--- app/src/main/java/com/zhoujian/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/zhoujian/kotlindemo/MainActivity.kt
@@ -9,5 +9,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        println(max(1,2))
+
+    }
+
+
+    fun max(a: Int, b: Int): Int {
+        return if (a > b) a else b
+    }
+
+    fun main(args: Array<String>) {
+        println("hello world")
     }
 }
